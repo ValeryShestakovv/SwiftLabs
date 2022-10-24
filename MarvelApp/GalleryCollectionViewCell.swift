e53a837870dbfd04cee3d574d7266fc773ab3e37
ValeryShestakovv <medicmtt@gmail.com>
2022-10-24 13:20:51 +0600

    added logo, text label, collectionView, background triangle
    added marvel logo, text label, implemented a collection view with animated scrolling, created a triangle view to display the figure in the background.

---
+++ MarvelApp/GalleryCollectionViewCell.swift
@@ -0,0 +1,42 @@
+import Foundation
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseId = "GalleryCollectionViewCell"
+    
+    public let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 16
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    let textLable: UILabel = {
+        let text = UILabel()
+        text.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        text.textColor = .white
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
+        return text
+    }()
+    var color: UIColor = .red
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(mainImageView)
+        addSubview(textLable)
+        mainImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        mainImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        textLable.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -40).isActive = true
+        textLable.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40).isActive = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,129 @@
+import SnapKit
+import UIKit
+import AnimatedCollectionViewLayout
+
+final class ViewController: UIViewController {
+
+    private let logo: UIImageView = {
+        let logo = UIImageView()
+        logo.image = UIImage(named: "logo")
+        logo.contentMode = .scaleAspectFit
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        return logo
+    }()
+    
+    private let label: UILabel = {
+        let textMarvel = UILabel()
+        textMarvel.text = "Choose your hero"
+        textMarvel.textColor = .white
+        textMarvel.font = UIFont(name: "HelveticaNeue-Bold", size: 30)
+        textMarvel.translatesAutoresizingMaskIntoConstraints = false
+        return textMarvel
+    }()
+    
+    private let figure = TriangleView()
+    
+    private lazy var galleryCollectionView: UICollectionView = {
+        let layout = AnimatedCollectionViewLayout()
+        layout.animator = LinearCardAttributesAnimator(minAlpha: 0.5, itemSpacing: 0.3, scaleRate: 0.8)
+        layout.minimumLineSpacing = Layout.galleryMinimumLineSpacing
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .none
+        collectionView.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseId)
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: Layout.leftDistanceToView, bottom: 0, right: Layout.rightDistanceToView)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    private var items: [HeroModel] = HeroModel.fetchHero()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        figure.backgroundColor = .red
+        self.view.backgroundColor = UIColor(red: 0.16, green: 0.15, blue: 0.17, alpha: 0.9)
+        view.addSubview(figure)
+        view.addSubview(logo)
+        view.addSubview(label)
+        view.addSubview(galleryCollectionView)
+
+        figure.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+ 
+        logo.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(Layout.verticalInset)
+            make.left.right.equalToSuperview().inset(Layout.horizontalInset)
+        }
+
+        label.snp.makeConstraints { make in
+            make.top.equalTo(logo.snp.bottom).inset(Layout.verticalInset)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+
+        galleryCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(label.snp.bottom).inset(10)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView is UICollectionView else {
+            return
+        }
+        let centerPoint = CGPoint(x: scrollView.frame.size.width / 2 + scrollView.contentOffset.x,
+                                  y: scrollView.frame.size.height / 2 + scrollView.contentOffset.y)
+        if let indexPath = galleryCollectionView.indexPathForItem(at: centerPoint) {
+            let item = items[indexPath.row]
+            figure.backgroundColor = item.color
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: Layout.galleryItemWidth, height: Layout.galleryItemHeight)
+    }
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseId, for: indexPath) as? GalleryCollectionViewCell else {
+            return .init()
+        }
+        let item = items[indexPath.row]
+        cell.mainImageView.image = item.image
+        cell.textLable.text = item.name
+        cell.color = item.color
+        return cell
+    }
+}
+
+// MARK: Layout Guides
+
+extension ViewController {
+    
+    enum Layout {
+        static var horizontalInset: CGFloat { 90 }
+        static var verticalInset: CGFloat { 20 }
+        static var leftDistanceToView: CGFloat { 5 }
+        static var rightDistanceToView: CGFloat { 0 }
+        static var galleryMinimumLineSpacing: CGFloat { 25 }
+        static let galleryItemWidth = (UIScreen.main.bounds.width - Layout.leftDistanceToView - Layout.rightDistanceToView - (Layout.galleryMinimumLineSpacing / 2))
+        static let galleryItemHeight = UIScreen.main.bounds.height - 150
+    }
+}
